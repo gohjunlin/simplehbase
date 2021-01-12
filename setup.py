b92a2099d7b86aef8b4d529cd2eaa280209fcb33
gohjunlin <62754326+gohjunlin@users.noreply.github.com>
2021-01-12 12:06:31 +0800

    Update setup.py
    Signed-off-by: gohjunlin <62754326+gohjunlin@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='simplehbase',
-      version='0.1',
+      version='0.2',
       description='A simple package to connect to HBase in Azure HDInsight using REST API',
       url='https://github.com/gohjunlin/simplehbase',
       author='Goh Jun Lin',
