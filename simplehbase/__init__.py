f6b91f7a60d821a73bcde9858d28baab50d4e7dd
gohjunlin <62754326+gohjunlin@users.noreply.github.com>
2021-02-05 11:22:41 +0800

    Update checking condition for get_value method to check if row_key exists.
    Signed-off-by: gohjunlin <62754326+gohjunlin@users.noreply.github.com>

--- simplehbase/__init__.py
+++ simplehbase/__init__.py
@@ -2,3 +2,5 @@ from ._version import __version__
 
 from .Azure_Hbase import AzHbaseRestAPI
 from .utils import df_to_dict
+from .utils import base64_to_string
+from .utils import string_to_base64
--- simplehbase/utils.py
+++ simplehbase/utils.py
@@ -1,15 +1,20 @@
 import base64
 from tqdm import tqdm
 
+def base64_to_string(x):
+    return base64.b64decode(x).decode()
+
+def string_to_base64(x):
+    return base64.b64encode(x.encode('utf-8')).decode()
+
 def df_to_dict(df):
     df = df.applymap(str, na_action='ignore') # Converting all values to string except for NaN
     df = df.melt(id_vars="ID").dropna().sort_values(by="ID").reset_index(drop = True).rename(columns = {'variable':'column', 'value':'$'})
-    df = df.applymap(lambda x: base64.b64encode(x.encode('utf-8')).decode())
+    # df = df.applymap(lambda x: base64.b64encode(x.encode('utf-8')).decode())
+    df = df.applymap(lambda x: string_to_base64(x))
+    df = df.set_index('ID')
     data = {'Row': []}
-    for key in tqdm(df["ID"].unique()):
-        Key = {'key': key, 'Cell':df.set_index("ID").loc[[key]].to_dict('records')}
+    for key in tqdm(df.index.unique()):
+        Key = {'key': key, 'Cell':df.loc[[key]].to_dict('records')}
         data['Row'].append(Key)
     return data
-
-def base64_to_string(x):
-    return base64.b64decode(x).decode()
