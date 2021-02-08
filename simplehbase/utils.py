1e48fe170aacc313721df71991a8897c3e544cea
gohjunlin <62754326+gohjunlin@users.noreply.github.com>
2021-02-08 10:10:55 +0800

    Update df_to_dict function, improving its speed of conversion.
    Signed-off-by: gohjunlin <62754326+gohjunlin@users.noreply.github.com>

--- simplehbase/utils.py
+++ simplehbase/utils.py
@@ -19,19 +19,34 @@ def string_to_base64(x):
 #         data['Row'].append(Key)
 #     return data
 
-def df_to_dict(df):
-    n_col = len(df.columns) - 1
-    num_list = [(0 + i) * (n_col) for i in range(len(df))]
+# def df_to_dict(df):
+#     n_col = len(df.columns) - 1
+#     num_list = [(0 + i) * (n_col) for i in range(len(df))]
+#
+#     df = df.fillna('').applymap(str, na_action='ignore')  # Converting all values to string except for NaN
+#     df = df.melt(id_vars="ID").dropna().sort_values(by="ID").reset_index(drop=True).rename(
+#         columns={'variable': 'column', 'value': '$'})
+#     # df = df.applymap(lambda x: base64.b64encode(x.encode('utf-8')).decode())
+#     df = df.applymap(lambda x: string_to_base64(x))
+#     df = df.set_index('ID')
+#
+#     #     data = {'Row': [{'key': key, 'Cell':df.loc[[key]].to_dict('records')} for key in tqdm(df.index.unique())]}
+#     data = {'Row': []}
+#     data = {'Row': [{'key': df.iloc[i: i + n_col].index[0], 'Cell': df.iloc[i: i + n_col].to_dict('records')} for i in
+#                     tqdm(num_list)]}
+#     return data
 
-    df = df.fillna('').applymap(str, na_action='ignore')  # Converting all values to string except for NaN
-    df = df.melt(id_vars="ID").dropna().sort_values(by="ID").reset_index(drop=True).rename(
-        columns={'variable': 'column', 'value': '$'})
-    # df = df.applymap(lambda x: base64.b64encode(x.encode('utf-8')).decode())
-    df = df.applymap(lambda x: string_to_base64(x))
+def df_to_dict(df):
+    df = df.fillna('').applymap(str)  # Convert NaN to empty string & change all values to string
+
+    n_col = len(df.columns) - 1
+    num_list = [i * n_col for i in range(len(df))]
+
+    df = df.melt(id_vars="ID").sort_values(by="ID").rename(columns={'variable': 'column', 'value': '$'}).applymap(
+        lambda x: string_to_base64(x))
     df = df.set_index('ID')
 
-    #     data = {'Row': [{'key': key, 'Cell':df.loc[[key]].to_dict('records')} for key in tqdm(df.index.unique())]}
-    data = {'Row': []}
     data = {'Row': [{'key': df.iloc[i: i + n_col].index[0], 'Cell': df.iloc[i: i + n_col].to_dict('records')} for i in
                     tqdm(num_list)]}
+
     return data
