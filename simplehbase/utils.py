04bfe32c63e5bfd7c7601a9185f22ed1012739b2
gohjunlin <62754326+gohjunlin@users.noreply.github.com>
2021-04-27 22:40:07 +0800

    Create new function - delete_row
    Signed-off-by: gohjunlin <62754326+gohjunlin@users.noreply.github.com>

--- simplehbase/utils.py
+++ simplehbase/utils.py
@@ -7,35 +7,6 @@ def base64_to_string(x):
 def string_to_base64(x):
     return base64.b64encode(x.encode('utf-8')).decode()
 
-# def df_to_dict(df):
-#     df = df.applymap(str, na_action='ignore') # Converting all values to string except for NaN
-#     df = df.melt(id_vars="ID").dropna().sort_values(by="ID").reset_index(drop = True).rename(columns = {'variable':'column', 'value':'$'})
-#     # df = df.applymap(lambda x: base64.b64encode(x.encode('utf-8')).decode())
-#     df = df.applymap(lambda x: string_to_base64(x))
-#     df = df.set_index('ID')
-#     data = {'Row': []}
-#     for key in tqdm(df.index.unique()):
-#         Key = {'key': key, 'Cell':df.loc[[key]].to_dict('records')}
-#         data['Row'].append(Key)
-#     return data
-
-# def df_to_dict(df):
-#     n_col = len(df.columns) - 1
-#     num_list = [(0 + i) * (n_col) for i in range(len(df))]
-#
-#     df = df.fillna('').applymap(str, na_action='ignore')  # Converting all values to string except for NaN
-#     df = df.melt(id_vars="ID").dropna().sort_values(by="ID").reset_index(drop=True).rename(
-#         columns={'variable': 'column', 'value': '$'})
-#     # df = df.applymap(lambda x: base64.b64encode(x.encode('utf-8')).decode())
-#     df = df.applymap(lambda x: string_to_base64(x))
-#     df = df.set_index('ID')
-#
-#     #     data = {'Row': [{'key': key, 'Cell':df.loc[[key]].to_dict('records')} for key in tqdm(df.index.unique())]}
-#     data = {'Row': []}
-#     data = {'Row': [{'key': df.iloc[i: i + n_col].index[0], 'Cell': df.iloc[i: i + n_col].to_dict('records')} for i in
-#                     tqdm(num_list)]}
-#     return data
-
 def df_to_dict(df):
     df = df.fillna('').applymap(str)  # Convert NaN to empty string & change all values to string
 
