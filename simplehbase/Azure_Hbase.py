88dbd469f32d0a0b485f38ccf1850adee0633fa8
gohjunlin <62754326+gohjunlin@users.noreply.github.com>
2021-04-27 23:02:50 +0800

    Update the new way of checking parameters
    Signed-off-by: gohjunlin <62754326+gohjunlin@users.noreply.github.com>

--- simplehbase/Azure_Hbase.py
+++ simplehbase/Azure_Hbase.py
@@ -14,62 +14,61 @@ class AzHbaseRestAPI:
                 'Accept': 'application/json',
                 'Content-Type': 'application/json',
             }
-    
+
+    def _checkParametersPresent(self):
+        if self.url == None or self.username == None or self.password == None:
+            print("Missing Parameters. Use connectionParamaters to set up URL, username and password")
+            return False
+        else:
+            return True
+
+
     def connectionParameters(self,url,username,password):
         self.url = url
         self.username = username
         self.password = password
     
     def create_table(self, table_name, columnFamilies):
-        if self.url == None or self.username == None or self.password == None:
-            print("Missing Parameters. Use connectionParamaters to set up URL, username and password")
+        if not self._checkParametersPresent():
             return
-        
-        else:
-            query_url = self.url+str(table_name)+'/schema'
-            ColumnSchema = []
-            for cf in columnFamilies:
-                ColumnSchema.append({"name":cf})
-            data = {"@name": table_name, "ColumnSchema":ColumnSchema}
 
+        query_url = self.url+str(table_name)+'/schema'
+        ColumnSchema = []
+        for cf in columnFamilies:
+            ColumnSchema.append({"name":cf})
+        data = {"@name": table_name, "ColumnSchema":ColumnSchema}
 
-            response = requests.put(query_url, headers=self.headers, data=json.dumps(data), auth=(self.username, self.password))
-            return response.status_code
+        response = requests.put(query_url, headers=self.headers, data=json.dumps(data), auth=(self.username, self.password))
+        return response.status_code
     
     def delete_table(self, table_name):
-        if self.url == None or self.username == None or self.password == None:
-            print("Missing Parameters. Use connectionParamaters to set up URL, username and password")
+        if not self._checkParametersPresent():
             return
-        
-        else:
-            query_url = self.url+str(table_name)+'/schema'
-            response = requests.delete(query_url, headers=self.headers, auth=(self.username, self.password))
-            return response.status_code
+
+        query_url = self.url+str(table_name)+'/schema'
+        response = requests.delete(query_url, headers=self.headers, auth=(self.username, self.password))
+        return response.status_code
     
     def get_table_schema(self, table_name):
-        if self.url == None or self.username == None or self.password == None:
-            print("Missing Parameters. Use connectionParamaters to set up URL, username and password")
+        if not self._checkParametersPresent():
             return
-        
-        else:
-            query_url = self.url+str(table_name)+'/schema'
-            response = requests.get(query_url, headers=self.headers, auth=(self.username, self.password))
-            return response.text
+
+        query_url = self.url+str(table_name)+'/schema'
+        response = requests.get(query_url, headers=self.headers, auth=(self.username, self.password))
+        return response.text
     
     def insert_data(self, table_name, data):
-        if self.url == None or self.username == None or self.password == None:
-            print("Missing Parameters. Use connectionParamaters to set up URL, username and password")
+        if not self._checkParametersPresent():
             return
-        
-        else:
-            query_url = self.url+str(table_name)+'/false-row-key"'
-            response = requests.put(query_url, headers=self.headers, data=json.dumps(data), auth=(self.username, self.password))
-            return response.status_code, response.reason
+
+        query_url = self.url+str(table_name)+'/false-row-key"'
+        response = requests.put(query_url, headers=self.headers, data=json.dumps(data), auth=(self.username, self.password))
+        return response.status_code, response.reason
     
     def get_value(self, table_name, row_key, column = None):
-        if self.url == None or self.username == None or self.password == None:
-            print("Missing Parameters. Use connectionParamaters to set up URL, username and password")
+        if not self._checkParametersPresent():
             return
+
         query_url = self.url + str(table_name) + "/" + str(row_key)
         response = requests.get(query_url, headers=self.headers, auth=(self.username, self.password))
         try:
@@ -90,18 +89,15 @@ class AzHbaseRestAPI:
                 return
 
     def delete_row(self, table_name, row_key):
-        if self.url == None or self.username == None or self.password == None:
-            print("Missing Parameters. Use connectionParamaters to set up URL, username and password")
+        if not self._checkParametersPresent():
             return
 
-        else:
-            query_url = self.url + str(table_name) + "/" + str(row_key)
-            response = requests.delete(query_url, headers=self.headers, auth=(self.username, self.password))
-            return response.status_code
+        query_url = self.url + str(table_name) + "/" + str(row_key)
+        response = requests.delete(query_url, headers=self.headers, auth=(self.username, self.password))
+        return response.status_code
 
     def create_scanner(self, table_name):
-        if self.url == None or self.username == None or self.password == None:
-            print("Missing Parameters. Use connectionParamaters to set up URL, username and password")
+        if not self._checkParametersPresent():
             return
 
         query_url = self.url + str(table_name) + "/scanner"
